0d1edd82bf23a3103282713d29886bc0f46665e9
Amir Djelidi <amir.djelidi@efrei.net>
2024-09-20 15:09:42 +0200

    Ex 3: remove vue CLI placeholders

---
+++ vue-oauth-microsoft-graph/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- You can add your own content here -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
