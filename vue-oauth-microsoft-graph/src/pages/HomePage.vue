31214ab44a49d2a06922b49f71bde2c9a663e766
Amir Djelidi <amir.djelidi@efrei.net>
2024-09-20 15:51:30 +0200

    Ex 4: create HomePage component

--- vue-oauth-microsoft-graph/src/pages/HomePage.vue
+++ vue-oauth-microsoft-graph/src/pages/HomePage.vue
@@ -1,5 +1,10 @@
 <template>
-
+<nav>
+  <div class="menu-item"><a href="#">Home</a> </div>
+  <div class="menu-item"><a href="#">I'm Yu</a> </div>
+  <div class="menu-item"><a href="#">He's me</a> </div>
+  <div class="menu-item"><a href="#">Don't contact me</a> </div>
+</nav>
   <div>
     <h1>Il n'y a pas de bonne ou de mauvaise Vue 😉</h1>
   </div>
@@ -13,9 +18,18 @@ export default {
 </script>
 
 <style scoped>
+nav {
+  display: flex;
+  background-color: green;
+  justify-content: space-around;
+}
+nav a{
+  color: white;
+  text-decoration: none;
+}
+
 h1 {
   text-align: center;
-
   font-family: "Bebas Neue", sans-serif;
   font-weight: 400;
   font-style: normal;
